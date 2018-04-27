0a93cc04ae18bd033183b2be79858b7ffa6cbff5
Daniel Simandl <dsimandl419@gmail.com>
2018-04-27 10:16:15 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { StackNavigator } from 'react-navigation';
+import HomeScreen from './screens/HomeScreen';
+import PredictScreen from './screens/PredictScreen';
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Prediction: {
+      screen: PredictScreen,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends Component {
+  render() {
+    return <RootStack />;
+  }
+}
---
+++ screens/PredictScreen/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Button
+} from 'react-native';
+import { Camera, Permissions } from 'expo';
+// import {MobileNet} from './models/mobilenet';
+// import * as tfc from '@tensorflow/tfjs-core';
+
+export default class PredictScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>PredictScreen</Text>
+        <Button
+          title="Go Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleText: {
+    fontSize: 50,
+  },
+});
